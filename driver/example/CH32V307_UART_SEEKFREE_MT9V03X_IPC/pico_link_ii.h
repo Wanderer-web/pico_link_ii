5b6e3c24f6e9c8c55679a28a624ab3701f560910
Wanderer <986224924@qq.com>
2023-04-04 20:30:02 +0800

    add ch32v307 example

---
+++ driver/example/CH32V307_UART_SEEKFREE_MT9V03X_IPC/pico_link_ii.h
@@ -0,0 +1,17 @@
+#ifndef _PICO_LINK_II_H__
+#define _PICO_LINK_II_H__
+
+#include "string.h"
+
+// 数据类型声明
+typedef unsigned char pico_uint8;
+typedef unsigned short int pico_uint16;
+typedef unsigned long int pico_uint32;
+
+void BaseSendBytes_Uart(pico_uint8 *pBuff, pico_uint32 len);
+void picoSendBytes_Uart(pico_uint8 *data, pico_uint32 length);
+void picoSendBytes_Spi(pico_uint8 *data, pico_uint32 length);
+void ipcSendImg_Uart(pico_uint8 *pImage, pico_uint32 imageH, pico_uint32 imageW);
+void ipcSendImg_Spi(pico_uint8 *pImage, pico_uint32 imageH, pico_uint32 imageW);
+
+#endif
